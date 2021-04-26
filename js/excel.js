b135c3f4c205f4e1c6c6b7d3adcaac6a9cde8559
PanfletoIG <panfletoindiegames@gmail.com>
2021-04-26 08:06:24 -0600

    actualizacion 1.26/04/2021

--- js/excel.js
+++ js/excel.js
@@ -6,6 +6,9 @@ var azar = 0;
 var columnas = 50;
 var comparar = "";
 var respuesta = "";
+var corte = "";
+var texto = [];
+var mescla = "";
 var points = 0;
 console.log("")
 
@@ -15,8 +18,6 @@ console.log("")
 readXlsxFile('../exel/uh.xlsx')
 .then((rows) => 
 {
-    //busca en la celda 1 y lo imprime
-    console.log(rows[0][0]+" "+rows[0][1])
     console.log("")
 
     //ciclo para el juego
@@ -26,30 +27,69 @@ readXlsxFile('../exel/uh.xlsx')
         aleatorio();
         
         //imprime acertijo
-        console.log(rows[azar][0]+". "+rows[azar][1]+" "+rows[0][3]);
+        console.log(rows[azar][0]+". "+rows[azar][1]+" "+rows[0][2]);
         
         //mete la respuesta
         respuesta = rows[azar][2];
+        respuesta = respuesta.toLocaleLowerCase();
+
+        //recorta
+        for(let i=0; i<respuesta.length; i++)
+        {
+            corte = respuesta.slice(i,i+1);
+            texto.push(corte);
+        }
+
+        //mescla el texto
+        for(let i = texto.length - 1; i>0; i--)
+        {
+            let iAleatorio = Math.floor(Math.random() * (i+1))
+            let temporal = texto[i];
+            texto[i] = texto[iAleatorio]
+            texto[iAleatorio] = temporal;
+        }
+        
+        //une el texto
+        for(let i = 0; i<respuesta.length; i++)
+        {
+            mescla = mescla.concat(texto[i] + " ");
+        }
+
+        console.log("   pista: "+"' "+mescla+"'");
 
         //pones tu respuesta
         //pondre opciones*******
         comparar = prompt("Tu respuesta => ")
         console.log("")
         
+        
         if(respuesta == comparar)
         {
             points++;
+            console.log("Acertaste");
+            console.log("");
         }
-        console.log(points)
-        console.log("")
+        else if(respuesta != comparar) 
+        {
+            console.log("La respuesta era: "+respuesta);
+            console.log("");
+        }
+        console.log(points);
+        console.log("");
+
+        //borra datos
+        texto = [];
+        mescla = "";
 
         //falta comparar*******
         if(points == 5)
         {
             parar = true;
+            console.log("Ganaste!!!");
+            console.log("");
         }
     }
-})
+});
 
 function aleatorio()
 {
